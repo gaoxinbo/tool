002ea24a2df339538977dcb72416be28a717532b
Administrator <gxb@ubuntu.ubuntu-domain>
2013-08-25 13:57:54 +0800

    fix some bug

--- run.sh
+++ run.sh
@@ -4,47 +4,51 @@ function green(){
   echo -e "\033[32m"$1
 }
 
+function yellow(){
+  echo -e "\033[33m"$1
+}
 function red(){
+
   echo -e "\033[31m"$1
 }
 function end(){
   echo -e "\033[0m"
 }
 
-red "installing cpp tool" 
+yellow "starting to install cpp tool" 
 end
 (cd cpp;make install >/dev/null 2>&1) 
 
 if [ "$?" != "0" ];then
-  red "install cpp tool" 
+  red "  install cpp tool error" 
   end
 else
-  green "install cpp successed"
+  green "  install cpp successed"
   end
 fi
 
-red "install go tool"
+yellow "starting to install go tool"
 end
 (cd go;./run.sh)
 
 if [ "$?" != "0" ];then
-  red "install go tool error"
+  red "  install go tool error"
   end
 else
-  green "install go tool succeed"
+  green "  install go tool succeed"
   end
 fi
 
 
-red "install python tool"
+yellow "starting to install python tool"
 end
 (cd python;./run.sh)
 
 if [ "$?" != "0" ];then
-  red "install py tool error"
+  red "  install py tool error"
   end
 else
-  green "install py tool succeed"
+  green "  install py tool succeed"
   end
 fi
 
